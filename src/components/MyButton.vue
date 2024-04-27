e2d1436a207389538dbb1bb849ad9e71d99cbecc
Efraingon <Efraingon@gmail.com>
2024-04-26 21:09:05 -0400

    Cambios cero

---
+++ src/components/MyButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button :style="{background, color}" :disabled="disabled">
+    Button
+  </button>
+
+</template>
+
+<script>
+import { String } from 'core-js';
+
+    export default {
+    name: 'MyButtonItem',
+     props:{
+            color:{
+                type:String
+            },
+            background:{
+                type:String
+            },
+            disable:{
+                type:Boolean
+            }
+
+     }        
+    }
+</script>
+
+
+<style scoped>
+    button:disabled{
+            opacity:0.5;
+    }
+    
+    button{
+        background: none;
+        color: black;
+        border: none;
+        border-radius: 5px;
+        padding: 10px 40px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+    button:hover{
+        filter: brightness(125%);
+    }      
+
+</style>
---
+++ src/components/Prueba.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+        <h3>
+            Acceso
+        </h3>
+   </div> 
+</template>
+
+<script>
+    export default {
+    name: 'PruebaItem'        
+    }
+</script>
+
+
+<style scoped>
+   div {
+    color:blue;
+
+   }    
+
+</style>
